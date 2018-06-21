afc67769b8021cc54805b9fe35eb66c037e5c21e
mokopo <mkp0824@i.softbank.jp>
2018-06-21 16:25:31 +0900

    クソザコシーン遷移

--- Assets/Scripts/ogisu/TitleGimmick.cs
+++ Assets/Scripts/ogisu/TitleGimmick.cs
@@ -5,11 +5,16 @@ using DG.Tweening;
 
 public class TitleGimmick : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        //transform.DOMoveX(8f,3.0f).SetLoops(-1);
-        //transform.DOLocalPath()
-	}
+    // Use this for initialization
+    void Start()
+    {
+        transform.DOLocalPath(
+            new Vector3[]
+            {
+                new Vector3(3f,3f),Vector3.zero,
+                new Vector3(-3f,2f)
+            }, 2f, PathType.CatmullRom).SetLoops(-1);
+    }
 	
 	// Update is called once per frame
 	void Update () {
