035d55fe4420ec4b2812ef659b59cc2a86a38dae
mokopo <mkp0824@i.softbank.jp>
2018-06-27 09:29:38 +0900

    インクアセットの追加とクリア演出

---
+++ Assets/InkPainter/Sample/Script/CollisionPainter.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+namespace Es.InkPainter.Sample
+{
+	[RequireComponent(typeof(Collider), typeof(MeshRenderer))]
+	public class CollisionPainter : MonoBehaviour
+	{
+
+
+		[SerializeField]
+		private Brush brush = null;
+
+		[SerializeField]
+		private int wait = 3;
+
+		private int waitCount;
+
+		public void Awake()
+		{
+			GetComponent<MeshRenderer>().material.color = brush.Color;
+		}
+
+		public void FixedUpdate()
+		{
+			++waitCount;
+		}
+
+		public void OnCollisionEnter(Collision collision)
+		{
+			if(waitCount < wait)
+				return;
+			waitCount = 0;
+
+			foreach(var p in collision.contacts)
+			{
+				var canvas = p.otherCollider.GetComponent<InkCanvas>();
+				if(canvas != null)
+					canvas.Paint(brush, p.point);
+			}
+            //Vector3 hitPos;
+            //foreach (ContactPoint point in collision.contacts)
+            //{
+            //    hitPos = point.point;
+
+            //    switch(collision.gameObject.name)
+            //    {
+            //        case "SphereFirst":
+            //            Instantiate(ClearCanvas, transform);
+            //            Debug.Log("FirstHit");
+            //            break;
+            //        case "SphereSecond":
+            //            break;
+            //        case "SphereThird":
+            //            break;
+            //        case "SphereFourth":
+            //            break;
+            //        case "SphereFifth":
+            //            break;
+            //    }
+
+
+            //    //Debug.Log(hitPos);
+            //}
+
+            gameObject.SetActive(false);
+		}
+    }
+}
---
+++ Assets/Scripts/ogisu/HitCheck.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HitCheck : MonoBehaviour {
+
+    public GameObject ClearCanvas;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        Vector3 hitPos;
+        foreach (ContactPoint point in collision.contacts)
+        {
+            hitPos = point.point;
+
+
+            switch (collision.gameObject.name)
+            {
+                
+
+                case "SphereFirst":
+                    //Instantiate(ClearCanvas, transform.position,transform.rotation);
+                    //Text text = ClearCanvas.GetComponentInChildren<Text>() as Text;
+                    //ClearCanvas.GetComponent<Text>().text = "C";
+
+                    Debug.Log("FirstHit");
+                    break;
+                case "SphereSecond":
+                    break;
+                case "SphereThird":
+                    break;
+                case "SphereFourth":
+                    break;
+                case "SphereFifth":
+                    break;
+            }
+            //Debug.Log(hitPos);
+        }
+    }
+}
